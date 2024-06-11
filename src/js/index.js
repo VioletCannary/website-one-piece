fb6c1eccbd02cfeb05e5d27518cabe85286d3a98
VioletCannary <daycaremoon22@gmail.com>
2024-06-11 16:57:25 -0300

    one piece

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,47 @@
 const botoes = document.querySelectorAll(".botao");
 const personagens = document.querySelectorAll(".personagem");
 
-botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
+let indiceSelecionado = 0;
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
+exibirPersonagem(indiceSelecionado);
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    indiceSelecionado = indice;
+    exibirPersonagem(indiceSelecionado);
+  });
 });
 
+window.addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    indiceSelecionado = (indiceSelecionado + 1) % personagens.length;
+  } else if (event.deltaY < 0) {
+    indiceSelecionado = (indiceSelecionado - 1 + personagens.length) % personagens.length;
+  }
+  exibirPersonagem(indiceSelecionado);
+});
+
+function exibirPersonagem(indice) {
+
+  desselecionarBotao();
+  desselecionarPersonagem();
+
+  botoes[indice].classList.add("selecionado");
+  personagens[indice].classList.add("selecionado");
+
+  const nomePersonagem = personagens[indice].querySelector(".nome-personagem");
+  const descricaoPersonagem = personagens[indice].querySelector(".descricao");
+
+  nomePersonagem.textContent = nomePersonagem.textContent;
+  descricaoPersonagem.textContent = descricaoPersonagem.textContent;
+}
+
 function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
+  const personagemSelecionado = document.querySelector(".personagem.selecionado");
+  personagemSelecionado.classList.remove("selecionado");
 }
 
 function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado.classList.remove("selecionado");
 }
